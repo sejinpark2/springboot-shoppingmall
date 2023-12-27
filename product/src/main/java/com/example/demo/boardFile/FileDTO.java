3b7ff5f5b6a36b1461097909f6f32b8de4e940ae
sejinpark2 <wlstp2367@gmail.com>
2023-12-27 10:12:39 +0900

    23.12.27
    boardFile 패키지 추가

---
+++ product/src/main/java/com/example/demo/boardFile/FileDTO.java
@@ -0,0 +1,42 @@
+package com.example.demo.DTO;
+
+import com.example.demo.entity.BoardFile;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+public class FileDTO {
+
+    // ** 파일 경로
+    private String filePath;
+
+    // ** 파일 이름
+    private String fileName;
+
+    // ** uuid (랜덤 키)
+    private String uuid;
+
+    // ** 파일 포맷
+    private String fileType;
+
+    // ** 파일 크기
+    private Long fileSize;
+
+    // **  게시물 id
+    private Long boardId;
+
+    public BoardFile toEntity() {
+        return BoardFile.builder()
+                .filePath(filePath)
+                .fileName(fileName)
+                .uuid(uuid)
+                .fileType(fileType)
+                .fileSize(fileSize)
+                .build();
+    }
+}
